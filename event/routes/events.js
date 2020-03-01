601af6229cb6b5eb71d692f96ee1ee0aa90ec168
Eduardo Garcia <eduardo.garcia@avature.net>
2020-03-01 16:54:04 +0100

    Move files to divide project by domain

--- event/routes/events.js
+++ event/routes/events.js
@@ -1,7 +1,7 @@
 const Event = require('../model/Event');
 const express = require('express');
 const router = express.Router();
-const eventService = require('../service/Event');
+const eventService = require('../../service/Event');
 
 router.get('/:id', function(req, res, next) {
   var service = new eventService();
