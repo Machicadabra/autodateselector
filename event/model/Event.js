653a9c0c504f3cfee74430dee0a1806c79939e13
Eduardo Garcia <eduardo.garcia@avature.net>
2020-03-02 21:41:26 +0100

    Add getters and setters to Event model

--- event/model/Event.js
+++ event/model/Event.js
@@ -5,6 +5,27 @@ class Event {
         this.durationInSeconds = durationInSeconds;
         this.attendees = attendees;
     }
+
+    getId() {
+        return this.id;
+    }
+
+    getDateInGMT() {
+        return this.dateInGMT;
+    }
+
+    getDurationInSeconds() {
+        return this.durationInSeconds;
+    }
+
+    getAttendees() {
+        return this.attendees;
+    }
+
+    getEndDateInGMT() {
+        let endTimeStamp = this.dateInGMT.getTime() + this.durationInSeconds * 1000;
+        return new Date(endTimeStamp);
+    }
 }
 
 module.exports = Event;
