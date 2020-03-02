3beeb7c5cd6960bab479284a209f3395ce5e353c
Eduardo Garcia <eduardo.garcia@avature.net>
2020-03-02 23:28:15 +0100

    Add factory method to Attendee

--- attendee/model/Attendee.js
+++ attendee/model/Attendee.js
@@ -1,4 +1,9 @@
 class Attendee {
+
+    static buildFromAttendee(attendee) {
+        return new Attendee(attendee.getId(), attendee.getName());
+    }
+
     constructor(id, name) {
         this.id = id;
         this.name = name;
