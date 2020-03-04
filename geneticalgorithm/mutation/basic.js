9abed8522de6e05318453defc79c499f8041b321
Eduardo Garcia <eduardo.garcia@avature.net>
2020-03-04 22:58:45 +0100

    Implement basic mutation

--- geneticalgorithm/mutation/basic.js
+++ geneticalgorithm/mutation/basic.js
@@ -1,4 +1,32 @@
+const DEFAULT_MUTATION_PROBABILITY = 0.01;
+const MAX_MILLISECONDS_TO_MUTATE = 900000;
+
 class Basic {
+
+    constructor(mutationProbability, maxMillisecondsToMutate) {
+         this.mutationProbability = isNaN(mutationProbability) ? DEFAULT_MUTATION_PROBABILITY : mutationProbability;
+         this.maxMillisecondsToMutate = isNaN(maxMillisecondsToMutate) ? MAX_MILLISECONDS_TO_MUTATE : maxMillisecondsToMutate;;
+    }
+
+    mutate(population) {
+        population.getIndividuals().array.forEach(individual => {
+            if (Math.random() < this.mutationProbability) {
+                this._mutateIndividual(individual);
+            }
+        });
+    }
+
+    _mutateIndividual(individual) {
+        let events = individual.getEvents();
+        let numberOfEvents = events.length;
+        this._mutateEvent(Math.round(Math.random() * numberOfEvents));
+    }
+
+    _mutateEvent(event) {
+        let randoSecondsToAdd = (this.maxMillisecondsToMutate * 2 * Math.random()) - this.maxMillisecondsToMutate;
+        let newDateInGMT = new Date(event.getDateInGMT().getTime() + randoSecondsToAdd);
+        event.setDateInGMT(newDateInGMT);
+    }
 
 }
 
