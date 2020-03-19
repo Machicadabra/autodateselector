c1121c7a96dee9b70804f977e5b80b1ddfcf3a5b
Eduardo Garcia <eduardo.garcia@avature.net>
2020-03-19 19:50:26 +0100

    Fix: define imported class in capital letters

--- event/routes/events.js
+++ event/routes/events.js
@@ -1,17 +1,17 @@
 const Event = require('../model/Event');
 const express = require('express');
 const router = express.Router();
-const eventService = require('../service/Event');
+const EventService = require('../service/Event');
 
 router.get('/:id', function(req, res, next) {
-  var service = new eventService();
+  var service = new EventService();
   service.getById(req.params.id).then(value => {
     res.send(value);
   })
 });
 
 router.get('/', function(req, res, next) {
-  var service = new eventService();
+  var service = new EventService();
   var loadedEvents = [];
   service.getAll().then(events => {
     loadedEvents = events;
@@ -20,13 +20,13 @@ router.get('/', function(req, res, next) {
       }
     )
     return Promise.all(promises);
-  }).then(attendees => {
+  }).then(() => {
     res.send(loadedEvents);
   });
 });
 
 router.post('/', function(req, res, next) {
-    var service = new eventService();
+    var service = new EventService();
     var event = new Event(req.body.id, new Date(req.body.dateInGMT), req.body.durationInSeconds, []);
     service.save(event);
 });
