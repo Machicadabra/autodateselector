d40e1fff3894c6d2293f9500e1209a31268d51f2
Eduardo Garcia <eduardo.garcia@avature.net>
2020-03-19 08:33:40 +0100

    Add script to intert data in AttendeeEvent table

---
+++ db/insertAttendeeEvent.sql
@@ -0,0 +1,32 @@
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (1, 1);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (1, 2);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (1, 3);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (1, 4);
+
+
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (2, 1);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (2, 4);
+
+
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (3, 2);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (3, 3);
+
+
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (4, 1);
+
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (5, 2);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (5, 4);
+
+
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (6, 1);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (6, 2);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (6, 3);
+
+
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (7, 1);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (7, 2);
+
+
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (8, 1);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (8, 2);
+INSERT INTO AttendeeEvent (eventId, attendeeId) VALUES (8, 4);
