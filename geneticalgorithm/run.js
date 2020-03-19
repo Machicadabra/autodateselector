362d44fe2ff8f4d028e7e094de98853f9a15de81
Eduardo Garcia <eduardo.garcia@avature.net>
2020-03-19 19:50:39 +0100

    Run genetic algorithm from data in database

--- geneticalgorithm/run.js
+++ geneticalgorithm/run.js
@@ -1,49 +1,16 @@
 const BasicScenario = require('./scenario');
-const Event = require('../event/model/Event');
-const Attendee = require('../attendee/model/Attendee');
+const EventService = require('../event/service/Event');
 
-let attendee1Paco = new Attendee(1, 'Paco');
-let attendee2Juan = new Attendee(2, 'Juan');
-let attendee3Carlos = new Attendee(3, 'Carlos');
-let attendee4Pepe = new Attendee(4, 'Pepe');
-
-let grupo1 = [
-    attendee1Paco,
-    attendee2Juan,
-    attendee3Carlos,
-    attendee4Pepe
-];
-let grupo2 = [
-    attendee1Paco,
-    attendee4Pepe
-];
-let grupo3 = [
-    attendee2Juan,
-    attendee4Pepe
-];
-let grupo4 = [
-    attendee1Paco,
-    attendee2Juan,
-    attendee3Carlos
-];
-
-let event1 = new Event(1, new Date(), 3600, grupo1);
-let event2 = new Event(1, new Date(), 3600, grupo2);
-let event3 = new Event(1, new Date(), 3600, grupo2);
-let event4 = new Event(1, new Date(), 3600, grupo1);
-let event5 = new Event(1, new Date(), 3600, grupo3);
-let event6 = new Event(1, new Date(), 3600, grupo4);
-
-
-
-var events = [
-    event1,
-    event2,
-    event3,
-    event4,
-    event5,
-    event6
-];
-
-var scenario = new BasicScenario(events, null, null, null, 100);
-scenario.run();
+var service = new EventService();
+var loadedEvents = [];
+service.getAll().then(events => {
+loadedEvents = events;
+let promises = events.map(event => {
+    return service.getAllAttendesByEventId(event.getId()).then(attendees => event.setAttendees(attendees));
+    }
+)
+return Promise.all(promises);
+}).then(() => {
+    var scenario = new BasicScenario(loadedEvents, null, null, null, 100);
+    scenario.run();
+});
